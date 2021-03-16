c7f062eb0d0d2ad1e1347cc512d85dc54fce99e4
gurushant-yb <gjidagi@yugabyte.com>
2021-03-16 11:36:28 +0530

    set default value to numKeysToRead->1500000,numKeysToWrite->2000000

--- src/main/java/com/yugabyte/sample/apps/CassandraKeyValueBase.java
+++ src/main/java/com/yugabyte/sample/apps/CassandraKeyValueBase.java
@@ -24,9 +24,9 @@ public abstract class CassandraKeyValueBase extends AppBase {
     appConfig.numReaderThreads = 24;
     appConfig.numWriterThreads = 2;
     // The number of keys to read.
-    appConfig.numKeysToRead = -1;
+    appConfig.numKeysToRead = 1500000;
     // The number of keys to write. This is the combined total number of inserts and updates.
-    appConfig.numKeysToWrite = -1;
+    appConfig.numKeysToWrite = 2000000;
     // The number of unique keys to write. This determines the number of inserts (as opposed to
     // updates).
     appConfig.numUniqueKeysToWrite = NUM_UNIQUE_KEYS;
